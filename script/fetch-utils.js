95054bef016a82752d0111523120191620c75ce4
armaBurton <armanarma@armaburton.com>
2022-01-11 15:48:54 -0800

    update template to include login by default

---
+++ script/fetch-utils.js
@@ -0,0 +1,33 @@
+export async function checkAuth() {
+    const user = await getUser();
+
+    if (!user) location.replace('../'); 
+}
+
+export async function redirectIfLoggedIn() {
+    if (await getUser()) {
+        location.replace('./workshop');
+    }
+}
+
+export async function signupUser(email, password){
+    const response = await client.auth.signUp({ email, password });
+    
+    return response.user;
+}
+
+export async function signInUser(email, password){
+    const response = await client.auth.signIn({ email, password });
+
+    return response.user;
+}
+
+export async function logout() {
+    await client.auth.signOut();
+
+    return window.location.href = '../';
+}
+
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
--- script/script.js
+++ script/script.js
@@ -1,8 +1,37 @@
-// import functions and grab DOM elements
+import { 
+    redirectIfLoggedIn, 
+    signInUser, 
+    signupUser,
+} from './fetch-utils.js';
 
-// let state
+const signInForm = document.getElementById('sign-in');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
+
+redirectIfLoggedIn();
+
+signUpForm.addEventListener('submit', async(event)=>{
+    event.preventDefault();
+    const user = await signupUser(signUpEmail.value, signUpPassword.value);
+
+    if (user){
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
+
+signInForm.addEventListener('submit', async(event)=>{
+    event.preventDefault();
+    const user = await signInUser(signInEmail.value, signInPassword.value);
+
+    if (user){
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
